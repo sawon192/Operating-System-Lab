c216d9aec661544acb29ad0b9752c6aacfa80878
M.Shawan Biswas <sawonbiswas110@gmail.com>
2026-06-24 20:43:30 +0600

    Create 5. LargestNumber.sh

---
+++ 05-Shell Scripting/5. LargestNumber.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Enter three numbers:"
+read a b c
+
+largest=$a
+
+if [ $b -gt $largest ]
+then
+    largest=$b
+fi
+
+if [ $c -gt $largest ]
+then
+    largest=$c
+fi
+
+echo "Largest Number = $largest"
