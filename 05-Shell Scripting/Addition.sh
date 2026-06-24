7f443ba3acb0348cedeca3d6498eae6209c9b7c4
M.Shawan Biswas <sawonbiswas110@gmail.com>
2026-06-24 20:30:19 +0600

    Create Addition.sh

---
+++ 05-Shell Scripting/Addition.sh
@@ -0,0 +1,17 @@
+#1. HelloWorld.sh
+#!/bin/bash
+
+echo "Hello World!"
+echo "Welcome to Shell Scripting."
+# 2. Addition.sh
+#!/bin/bash
+
+echo "Enter first number:"
+read a
+
+echo "Enter second number:"
+read b
+
+sum=$((a+b))
+
+echo "Sum = $sum"
